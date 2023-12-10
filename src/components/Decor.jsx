6a32237f2bf3b1a071945bd4f2330246b17ce146
james1821 <jamesespinosamark@.com>
2023-12-10 14:19:06 +0800

    Added swiper library

--- src/components/Decor.jsx
+++ src/components/Decor.jsx
@@ -4,25 +4,58 @@ import { faBox } from "@fortawesome/free-solid-svg-icons";
 import { faPaw } from "@fortawesome/free-solid-svg-icons";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
 import { Slide, Fade } from "react-awesome-reveal";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
 function Decor() {
   return (
     <div>
       <div className="md:grid grid-cols-2  text-center md:px-20 pt-10 ">
-        <div
-          className="relative h-[200px] md:h-[500px] bg-cover flex flex-col items-center justify-center "
-          style={{
-            backgroundImage:
-              'url("https://res.cloudinary.com/luxuryp/images/w_1280,c_limit,f_auto,q_auto/ghtq7jtce7tsgdjtosrs/image-025")',
-          }}
+        <Swiper
+          spaceBetween={30}
+          slidesPerView={1}
+          autoplay={{ delay: 3000 }}
+          modules={[Autoplay]}
         >
-          <Slide>
-            <div className="pt-20">
-              <h1 className="text-4xl font-bold md:text-6xl text-white ">
-                Decor Guidance
-              </h1>
+          <SwiperSlide key="1">
+            <div
+              className=" h-[200px] md:h-[500px] bg-cover flex flex-col items-center justify-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/luxuryp/images/w_1280,c_limit,f_auto,q_auto/ghtq7jtce7tsgdjtosrs/image-025")',
+              }}
+            >
+              <Slide>
+                <div className="pt-20">
+                  <h1 className="text-4xl font-bold md:text-6xl text-white">
+                    Decor Guidance
+                  </h1>
+                </div>
+              </Slide>
             </div>
-          </Slide>
-        </div>
+          </SwiperSlide>
+
+          <SwiperSlide key="2">
+            {" "}
+            {/* Use a unique key, e.g., "2" */}
+            <div
+              className=" h-[200px] md:h-[500px] bg-cover flex flex-col items-center justify-center"
+              style={{
+                backgroundImage:
+                  'url("https://res.cloudinary.com/luxuryp/images/w_1280,c_limit,f_auto,q_auto/axx0whb7xfmlwhvcckrr/image-023")', // Provide the URL for the second image
+              }}
+            >
+              <Slide>
+                <div className="pb-20">
+                  <h1 className="text-4xl font-bold md:text-6xl text-black bg-white">
+                    Intentional Layout
+                  </h1>
+                </div>
+              </Slide>
+            </div>
+          </SwiperSlide>
+        </Swiper>
 
         <div className="">
           <h1 className=" text-4xl font-bold py-5 lg:text-6xl  text-black ">
